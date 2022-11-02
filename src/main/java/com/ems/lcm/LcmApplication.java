f00f96e64057bc67cca1c1b28fa0e72a4b0c6e2b
azaraelcorpin <azarael.corpin@msugensan.edu.ph>
2022-11-02 14:48:55 +0800

    setting up loggers

--- src/main/java/com/ems/lcm/LcmApplication.java
+++ src/main/java/com/ems/lcm/LcmApplication.java
@@ -3,11 +3,19 @@ package com.ems.lcm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 @SpringBootApplication
 public class LcmApplication {
 
+	private static final Logger LOGGER = LogManager.getLogger(LcmApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(LcmApplication.class, args);
+
+		LOGGER.info("Info level log message");
+        LOGGER.debug("Debug level log message");
+        LOGGER.error("Error level log message");
 	}
 
 }
