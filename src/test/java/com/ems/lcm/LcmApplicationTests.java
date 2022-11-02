071afeb70ee8337530836e2defe48b4c93e2bbd6
azaraelcorpin <azarael.corpin@msugensan.edu.ph>
2022-11-02 14:40:14 +0800

    initialize

---
+++ src/test/java/com/ems/lcm/LcmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ems.lcm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LcmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
